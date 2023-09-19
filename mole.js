ee5a534490b8df623c4a2ef243957e357063d4ba
Tony Dayton <tonylynndayton@yahoo.com>
2023-09-18 22:27:23 -0600

    Added moles

--- mole.js
+++ mole.js
@@ -2,6 +2,7 @@ window.onload = function () {
   setGame();
 };
 
+let currMoleTile;
 function setGame() {
   //set up the grid for the game board in html
   for (let i = 0; i < 9; i++) {
@@ -14,4 +15,22 @@ function setGame() {
     document.getElementById("board").appendChild(tile);
     // that's what tells wherrrre to place the created divs (document.createElement("div")-- so the board is the parent, and the tiles are children under the board ID
   }
+  setInterval(setMole, 2000); //2000 milliseconds
+}
+
+function getRandomTile() {
+  let num = Math.floor(Math.random() * 9);
+  return num.toString();
+}
+
+function setMole() {
+  if (currMoleTile) {
+    currMoleTile.innerHTML = "";
+  }
+  let mole = document.createElement("img");
+  mole.src = "./assets/monty-mole.png";
+  //mole is a variable that is assigned to the image
+  let num = getRandomTile();
+  currMoleTile = document.getElementById(num);
+  currMoleTile.appendChild(mole);
 }
